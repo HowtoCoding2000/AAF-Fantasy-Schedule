337de76ec17065ebb6e50c86842d771a459b2c13
calvin.farrell <calvin.farrell@MU-C1-12.mullen.pvt.k12.co.us>
2019-02-13 15:06:46 -0700

    intial commit

---
+++ AAF FAntasy Schedule/src/FantasySchedule.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+public class FantasySchedule
+	{
+		public static void main(String[] args)
+			{
+				ArrayList<Team> teams = new ArrayList<Team>();
+				teams.add(new Team("Hotshots", "Arizona", "Western Conference", 1, 0, 0));
+				teams.add(new Team("Stallions", "Salt Lake", "Western Conference", 0, 0, 1));
+				teams.add(new Team("Commanders", "San Antonio", "Western Conference", 1, 0, 0));
+				teams.add(new Team("Fleet", "San Diego", "Western Conference", 0, 0, 1));
+				teams.add(new Team("Legends", "Atlanta", "Eastern Conference", 0, 0, 1));
+				teams.add(new Team("Iron", "Birmingham", "Eastern Conference", 1, 0, 0));
+				teams.add(new Team("Express", "Memphis", "Eastern Conference", 0, 0, 1));
+				teams.add(new Team("Apollos", "Orlando", "Eastern Conference", 1, 0, 0));
+				int i = 0;
+				boolean conference2Used = false;
+				for (Team t : teams)
+					{
+						if (i == 0)
+							{
+								System.out.println(teams.get(i).getConference() + ":");
+							}
+						else if (teams.get(i).getConference() != teams.get(0).getConference())
+							{
+								if (conference2Used == false)
+									{
+										System.out.println();
+										System.out.println(teams.get(i).getConference() + ":");
+										conference2Used = true;
+									}
+							}
+						System.out.print(teams.get(i).getCity() + " " + teams.get(i).getName() + "  " + teams.get(i).getWins() + "-");
+						if (teams.get(i).getTies() > 0)
+							{
+								System.out.print(teams.get(i).getTies() + "-");
+							}
+						System.out.print(teams.get(i).getLosses());
+						System.out.println();
+						i++;
+					}
+			}
+	}
---
+++ AAF FAntasy Schedule/src/Team.java
@@ -0,0 +1,68 @@
+
+public class Team
+	{
+		protected String name;
+		protected String city;
+		protected String conference;
+		protected int wins;
+		protected int ties;
+		protected int losses;
+		public Team(String n, String c, String co, int w, int t, int l)
+			{
+				name = n;
+				city = c;
+				conference = co;
+				wins = w;
+				ties = t;
+				losses = l;
+			}
+		public String getName()
+			{
+				return name;
+			}
+		public void setName(String name)
+			{
+				this.name = name;
+			}
+		public String getCity()
+			{
+				return city;
+			}
+		public void setCity(String city)
+			{
+				this.city = city;
+			}
+		public String getConference()
+			{
+				return conference;
+			}
+		public void setConference(String conference)
+			{
+				this.conference = conference;
+			}
+		public int getWins()
+			{
+				return wins;
+			}
+		public void setWins(int wins)
+			{
+				this.wins = wins;
+			}
+		public int getTies()
+			{
+				return ties;
+			}
+		public void setTies(int ties)
+			{
+				this.ties = ties;
+			}
+		public int getLosses()
+			{
+				return losses;
+			}
+		public void setLosses(int losses)
+			{
+				this.losses = losses;
+			}
+		
+	}
