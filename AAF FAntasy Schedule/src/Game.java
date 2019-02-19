ae82b23e8a34166eaec39aac77f6bed8a24c478d
calvin.farrell <calvin.farrell@MU-C1-12.mullen.pvt.k12.co.us>
2019-02-19 15:28:31 -0700

    addGame method works without idiot proofing

--- AAF FAntasy Schedule/src/Game.java
+++ AAF FAntasy Schedule/src/Game.java
@@ -2,10 +2,10 @@ import java.util.ArrayList;
 import java.util.Scanner;
 public class Game
 	{
-		protected Scanner userInput = new Scanner(System.in);
-		protected int week;
-		protected Team homeTeam;
-		protected Team awayTeam;
+		public static Scanner userInput;
+		public static int week;
+		public static Team homeTeam;
+		public static Team awayTeam;
 		public Game(int w, Team h, Team a)
 			{
 				week = w;
@@ -14,6 +14,7 @@ public class Game
 			}
 		public static void addNewGame()
 		{
+			userInput = new Scanner(System.in);
 			System.out.println("Who is the home team?");
 			int i = 0;
 			for(Team t: FantasySchedule.teams)
@@ -21,6 +22,54 @@ public class Game
 					System.out.println("(" + (i + 1) + ")" + FantasySchedule.teams.get(i).getName());
 					i++;
 				}
+			int userHomeChoice = userInput.nextInt() - 1;
+			homeTeam = FantasySchedule.teams.get(userHomeChoice);
+			System.out.println("Who is the away team");
+			int j = 0;
+			int k = 0;
+			for(Team t: FantasySchedule.teams)
+				{
+					if (j != userHomeChoice)
+						{
+							System.out.println("(" + (k + 1) + ")" + FantasySchedule.teams.get(j).getName());
+							j++;
+							k++;
+						}
+					else
+						{
+							j++;
+						}
+				}
+			int userAwayChoice = userInput.nextInt() - 1;
+			awayTeam = FantasySchedule.teams.get(userAwayChoice);
+			FantasySchedule.games.add(new Game(FantasySchedule.games.size() + 1,homeTeam, awayTeam));
+			System.out.print(FantasySchedule.games.get(FantasySchedule.games.size() - 1).getWeek());
+			System.out.print(FantasySchedule.games.get(FantasySchedule.games.size() - 1).getHomeTeam());
+			System.out.print(FantasySchedule.games.get(FantasySchedule.games.size() - 1).getAwayTeam());
 		}
+		public int getWeek()
+			{
+				return week;
+			}
+		public void setWeek(int week)
+			{
+				this.week = week;
+			}
+		public Team getHomeTeam()
+			{
+				return homeTeam;
+			}
+		public void setHomeTeam(Team homeTeam)
+			{
+				this.homeTeam = homeTeam;
+			}
+		public Team getAwayTeam()
+			{
+				return awayTeam;
+			}
+		public void setAwayTeam(Team awayTeam)
+			{
+				this.awayTeam = awayTeam;
+			}
 	}
 
